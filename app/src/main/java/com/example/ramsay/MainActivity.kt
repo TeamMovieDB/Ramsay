c529ef3873dd17d0981ef799f5312bcecba2a256
AlikhanBaisholanov <uhuput07@gmail.com>
2020-07-05 16:02:43 +0600

    Add collapsing toolbar and recycler view to restaurants fragment

--- app/src/main/java/com/example/ramsay/MainActivity.kt
+++ app/src/main/java/com/example/ramsay/MainActivity.kt
@@ -2,18 +2,23 @@ package com.example.ramsay
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.ramsay.model.Restaurant
-import com.example.ramsay.ui.RestaurantItem
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.FragmentManager
+import com.example.ramsay.fragments.RestaurantFragment
 
 class MainActivity : AppCompatActivity() {
+    private val fragmentManager: FragmentManager = supportFragmentManager
+    private val restaurantsFragment = RestaurantFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bindViews()
         givingFakeData()
+        fragmentManager.beginTransaction().add(R.id.frame, restaurantsFragment).commit()
     }
 
     private fun bindViews() {
+
     }
 
     private fun givingFakeData() {
